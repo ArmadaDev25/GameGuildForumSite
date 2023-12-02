2ab195b97ffa477221ed8c2b46d81f488a0041b0
ArmadaDev25 <119649188+ArmadaDev25@users.noreply.github.com>
2023-12-02 05:14:46 -0700

    Created the GuildInfo data model

--- forumsite_project/main_app/models.py
+++ forumsite_project/main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class GuildInfo(models.Model):
+    name = models.CharField(max_length=60)
+    ticker = models.CharField(max_length=6)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+
