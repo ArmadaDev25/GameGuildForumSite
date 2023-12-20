a41ea00b87577152d61e05dd6ed194539340cf12
ArmadaDev25 <119649188+ArmadaDev25@users.noreply.github.com>
2023-12-19 19:37:59 -0700

    Deleted the Landing page and added imports for auth on the views

--- forumsite_project/main_app/views.py
+++ forumsite_project/main_app/views.py
@@ -2,11 +2,19 @@ from django.shortcuts import render
 from .models import GuildInfo
 from django.views.generic.base import RedirectView
 from django.views.generic import DetailView
+
+# User Login/Registration imports
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
+# Auth Requirement Imports
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+
 # Create your views here.
 
-class LandingPage(DetailView):
-    model = GuildInfo
-    template_name = "landingpage.html"
+
 
 class LandingPageRedirect(RedirectView):
     permanent = True
